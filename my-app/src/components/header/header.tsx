eaa0a5acaf33cc7ccd081ecffad5f6a6c6d183f4
Yaroslav Tsybukh <yaroslav.tsybukh@gmail.com>
2023-07-26 17:04:22 +0300

    work with header mob/desktop , work with component userPost

--- my-app/src/components/header/header.tsx
+++ my-app/src/components/header/header.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import { AppBar, Box , Toolbar , IconButton , Typography , Menu , Container , Button , MenuItem}  from '@mui/material';
+import { AppBar, Box , Toolbar , IconButton , Typography , Menu , Container , Button , MenuItem , Link}  from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-import AdbIcon from '@mui/icons-material/Adb';
+
+import { Link as RouterLink } from "react-router-dom";
 
 const pages = ['Products', 'Pricing', 'Blog'];
 export const Header = () => {
@@ -17,27 +18,29 @@ export const Header = () => {
     return (
         <AppBar position="static">
             <Container maxWidth="xl">
-                <Toolbar disableGutters>
-                    <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="a"
-                        href="/"
-                        sx={{
-                            mr: 2,
-                            display: { xs: 'none', md: 'flex' },
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            letterSpacing: '.3rem',
-                            color: 'inherit',
-                            textDecoration: 'none',
-                        }}
-                    >
-                        LOGO
-                    </Typography>
-
-                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                <Toolbar disableGutters sx={{ justifyContent: 'space-between'}}>
+                    <Box sx={{display: {xs: 'none' , md: 'block'}}}>
+                        <Link color='inherit' underline='none' component={RouterLink} to="/">
+                            LOGO
+                        </Link>
+                    </Box>
+                    <Box sx={{display: {xs: 'block' , md: 'none'}}}>
+                        <Link color='inherit' underline='none' component={RouterLink} to="/">
+                            LOGO
+                        </Link>
+                    </Box>
+                    <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+                        {pages.map((page) => (
+                            <Button
+                                key={page}
+                                onClick={handleCloseNavMenu}
+                                sx={{ my: 2, color: 'white', display: 'block' }}
+                            >
+                                {page}
+                            </Button>
+                        ))}
+                    </Box>
+                    <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
                         <IconButton
                             size="large"
                             aria-label="account of current user"
@@ -72,36 +75,6 @@ export const Header = () => {
                                 </MenuItem>
                             ))}
                         </Menu>
-                    </Box>
-                    <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-                    <Typography
-                        variant="h5"
-                        noWrap
-                        component="a"
-                        href=""
-                        sx={{
-                            mr: 2,
-                            display: { xs: 'flex', md: 'none' },
-                            flexGrow: 1,
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            letterSpacing: '.3rem',
-                            color: 'inherit',
-                            textDecoration: 'none',
-                        }}
-                    >
-                        LOGO
-                    </Typography>
-                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                        {pages.map((page) => (
-                            <Button
-                                key={page}
-                                onClick={handleCloseNavMenu}
-                                sx={{ my: 2, color: 'white', display: 'block' }}
-                            >
-                                {page}
-                            </Button>
-                        ))}
                     </Box>
                 </Toolbar>
             </Container>
--- my-app/src/components/userPost/userPost.tsx
+++ my-app/src/components/userPost/userPost.tsx
@@ -1,18 +1,23 @@
 import { IUserPost } from "../../shared/userPosts.interface.ts";
-import { Box } from '@mui/system';
-import { Typography } from '@mui/material';
+import { Typography, CardActions, CardContent , Button , Card } from '@mui/material';
+
 
 export const UserPost = (props: IUserPost) => {
     const { body, title  } = props
 
     const handleClick = () => {
-
+        console.log(1)
     }
 
     return (
-        <Box sx={{boxShadow:'2px 2px 10px red', border: '1px solid black;' , padding: '20px'}} onClick={() => handleClick()}>
-            <Typography sx={{ fontSize: '20px' , fontWeight: 'bold'}} gutterBottom={true} variant={'h2'}>{title}</Typography>
-            <Typography sx={{ fontSize: '18px'}} paragraph={true}>{body}</Typography>
-        </Box>
+        <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+                <Typography sx={{ fontSize: '20px' , fontWeight: 'bold'}} gutterBottom={true} variant={'h2'}>{title}</Typography>
+                <Typography sx={{ fontSize: '18px'}} paragraph={true}>{body}</Typography>
+            </CardContent>
+            <CardActions>
+                <Button onClick={handleClick    } size="small">Learn More</Button>
+            </CardActions>
+        </Card>
     )
 }
